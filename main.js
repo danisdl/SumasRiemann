5a37d6789797c79c3cc2045d911d953e5237c0d1
unknown <danielasdl0207@outlook.com>
2019-07-05 11:06:06 -0500

    commit fin de clase

--- main.js
+++ main.js
@@ -2,4 +2,16 @@
 //Por: Jesús Armando Mercado Lugo
 //Daniela solis de leon
 //Itzel Garcia Avila
+var n;
+var difx = (b - a)/n;
 
+var boton = document.getElementById("btn1");
+
+boton.addEventListener("click", main);
+
+function main()
+{
+    var fun = parseInt(document.getElementById("in1").value);
+    var a = parseInt(document.getElementById("rangoA").value);
+    var b = parseInt(document.getElementById("rangoB").value);
+}
